951590aa01a2e03fe2603f900c66a8ed0462bb1b
pbsnr <pbsnr.dev@gmail.com>
2020-12-16 07:59:56 +0100

    app now does generate data and we get rid of pickle

--- app.py
+++ app.py
@@ -16,14 +16,66 @@ import pickle
 
 app = Flask(__name__)
 
+n_tweets_to_read = 15000 # Choose the number of tweets to read
+
 cosine_similarity = lambda a, b: np.inner(a, b) / norm(a) * norm(b) if norm(a) != 0.0 and norm(b) != 0.0 else 0.0
+
+def TermDocumentMatrix(docs, docIDs=None):
+    vectorizer = CountVectorizer(lowercase=True, stop_words=None)
+    tdm = vectorizer.fit_transform(docs)
+    tdm_feature_names = vectorizer.get_feature_names()
+    #
+    df = pd.DataFrame(tdm.toarray(), columns=tdm_feature_names, dtype="float64")
+    if docIDs is not None:
+        df.index = docIDs    
+    return df
+
+#Initialisation
 ps = PorterStemmer()
-with open('save.pickle', 'rb') as f:
-        matrix = pickle.load(f)
-with open('cleantweet.pickle', 'rb') as f1:
-    cleantweet = pickle.load(f1)
-with open('tweetids.pickle', 'rb') as f2:
-    tweetIds = pickle.load(f2)
+
+# Read the data and remove duplicates if exist
+tweets = []
+with open("data/data.txt", encoding="utf-8") as file:
+    for i, line in enumerate(file):
+        if i < n_tweets_to_read:
+            tweets.append(line)
+        else:
+            break  
+tweets = list(set(tweets)) # delete duplicates
+print("{} unique Tweets loaded\n".format(len(tweets)))
+
+# Data Cleanning and prepocesing phase
+cleanTweet = []
+tweetIDs = []
+tweetsProcessed = []
+for tweet in tweets:
+    try:
+        doc = tweet.split("\t") 
+        if len(doc)==7:
+            cleanTweet.append(doc[5])
+            tweetIDs.append(doc[0])     # add ID to a list
+            doc = doc[5:6]   # remove everything that's note the tweet content      
+            tok_doc = word_tokenize(" ".join(doc))    # tokenize remaining document
+            stemmed_doc = [ps.stem(word) for word in tok_doc] 
+            tweetsProcessed.append(" ".join(stemmed_doc))   #stemmed words
+    except:
+        pass
+
+# TF-IDF construction part
+
+# Terms frequency
+tdf = TermDocumentMatrix(tweetsProcessed, tweetIDs)
+
+# Document frequency
+documentFrequencies = []
+for index, series in tdf.iteritems(): 
+    documentFrequencies.append(len(series.nonzero()[0])) 
+
+# TF-IDF weight
+tdf.applymap(lambda x: 1.0 + np.log10(x) if x > 0.0 else 0.0) # log frequency weight
+idf = pd.Series(np.log10(len(tweets)/np.array(documentFrequencies))) # Inverse document frequency
+matrix = tdf * idf.values
+
 
 def printTopSimilarTweets(tf_idf, tweet, cleanTweet, tweetIDs, n=20):
 
@@ -59,7 +111,7 @@ def index():
     
     if request.method == 'POST':
         details = request.form
-        res =  printTopSimilarTweets(matrix, details['text-to-analyse'], cleantweet, tweetIds)
+        res =  printTopSimilarTweets(matrix, details['text-to-analyse'], cleanTweet, tweetIDs)
     return render_template('index.html', result = res )
 
 if __name__ == '__main__':
