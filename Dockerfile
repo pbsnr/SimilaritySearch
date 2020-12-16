98d3fcb4cbad7b728b1b7231922e5e5317be0d79
Balane Brenden <brenden.balane@efrei.net>
2020-12-16 08:53:11 +0100

    add docker files for containerization

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6
+
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+ENV FLASK_APP=app.py
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
---
+++ run.sh
@@ -0,0 +1,19 @@
+python test_app.py
+
+gnome-terminal --tab -- bash -c "sudo docker-compose up"
+
+
+while true
+do
+    wget -q --spider http://localhost:5000
+
+ 
+
+    if [ $? -eq 0 ]; then
+        echo "Online"
+        break
+    else
+        echo "Offline"
+        sleep 5
+    fi
+done
