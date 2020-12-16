27c1c9abd836a42d799f1fcd14109164f6789a8d
brendenbln <45016455+brendenbln@users.noreply.github.com>
2020-12-16 10:06:04 +0100

    Update jenkins.release.groovy

--- jenkins.release.groovy
+++ jenkins.release.groovy
@@ -1,5 +1,5 @@
 def build_app(){
-  sh 'sudo docker-compose up -d'
+  sh 'docker-compose up -d'
 }
 
 
@@ -15,7 +15,7 @@ def stress_test_app(){
 }
 
 def down_app(){
-  sh 'sudo docker-compose down'
+  sh 'docker-compose down'
 }
 
 def release_app(){
