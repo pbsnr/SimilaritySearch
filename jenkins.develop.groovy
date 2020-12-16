5445b2f3e45a9984291306ce1f471321e4849041
brendenbln <45016455+brendenbln@users.noreply.github.com>
2020-12-16 12:54:40 +0100

    Update jenkins.develop.groovy

--- jenkins.develop.groovy
+++ jenkins.develop.groovy
@@ -2,10 +2,10 @@ def build_app(){
   sh 'docker-compose up -d'
 }
 
-def test_app(){
+/*def test_app(){
   sh 'sleep 60s'
   sh 'python test_app.py'
-}
+}*/
 
 def stress_test_app(){
   //sh 'docker run -p 8089:8089 -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/locustfile.py'
